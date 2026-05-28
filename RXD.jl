72bf98a16633827dfd2e77016e420c1eb59f1ce3
MarcosViniciusBurdzinski <marcosviniciusburdzinskim@gmail.com>
2026-05-28 17:38:10 -0300

    Explicação teórica do porque o condiconamento das matrizes de L e R são iguais

--- RXD.jl
+++ RXD.jl
@@ -115,9 +115,32 @@ function teste_cond_m()
             x[j] = j
         end
 
-        d[i - 5] = log(cond_Lx(x, i, 5))
+        d[i - 5] = log(cond(x, i, 5))
     end
 
     plot(numero_pontos, d, marker=:circle)
 end
-teste_cond_m()
+teste_cond()
+
+#=
+# Letra d)
+
+Lema: O condicionamento da matriz de R_{Xd} é igual ao condicionamento da matriz de L_{X}
+
+L_{X} na base canônica de Pd é a matriz V de vandermonde, onde V_{ij} = x_{i}^{j-1}. Também,
+R_{Xd} é a pseudo-inversa da matriz de L_{X}, ou seja, (V^{T}V)^{-1}V^{T} = V†. Seja s_{1}, ..., s_{n}
+os valores singulares de V, com s_{1} ≥ s_{2} ≥ ... ≥ s_{n}. Então existem listas ortonormais e_{1}, ..., e_{n} e f_{1}, ..., f_{n}
+tais que:
+Vu = s_{1}\left\langle u, e_{1} \right\rangle f_{1} + ... + s_{n} \left\langle u, e_{n} \right\rangle f_{n}
+e
+V†u = \frac{\left\langle u, f_{1} \right\rangle}{s_{1}} e_{1} + ... + \frac{\left\langle u, f_{n} \right\rangle}{s_{n}} e_{n}
+
+Sabemos que o condicionamento de uma matriz é o maior valor singular dividido pelo menor. Então
+k(V) = \frac{s_{1}}{s_{n}}
+Enquanto isso, o maior valor singular de V† é \frac{1}{s_{n}}, enquanto o menor é \frac{1}{s_{1}}. Logo,
+k(V†) = \frac{\frac{1}{s_{n}}} {\frac{1}{s_{1}}} = \frac{s_{1}}{s_{n}} = k(V)
+
+Logo, os condicionamentos são iguais
+Desse modo, basta analisar o comportamento do condicionamento da matriz de L_{X} quando d e m variam, 
+pois este será igual ao da matriz de R_{Xd}.
+=#
